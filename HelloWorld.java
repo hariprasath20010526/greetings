8ee4d146f84b6c397003de61793471e7b1eb1076
hariprasath20010526 <116069471+hariprasath20010526@users.noreply.github.com>
2022-10-18 02:19:14 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package hello.world;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		 System.out.println("hai");
+		 System.out.println("HELLO");
+		 System.out.println("hey");
+		 System.out.println("hehe");
+		 
+	}
+
+}
